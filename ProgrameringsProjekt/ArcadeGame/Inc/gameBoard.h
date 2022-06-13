babde49c320cea3a18d1d51e54b95892ef3d51f1
s214399 <s214399@student.dtu.dk>
2022-06-13 12:21:59 +0200

    gameboard
    makes the outline of the board

---
+++ ProgrameringsProjekt/ArcadeGame/Inc/gameBoard.h
@@ -0,0 +1,12 @@
+#ifndef GAMEBOARD_H_
+#define GAMEBOARD_H_
+#include "30010_io.h"
+#include "stdint.h"
+#include "stdio.h"
+#include "ansi.h"
+#include "ex2.h"
+
+void makeBoard(void);
+
+
+#endif /* GAMEBOARD_H_ */
---
+++ ProgrameringsProjekt/ArcadeGame/Src/gameBoard.c
@@ -0,0 +1,29 @@
+#include "gameBoard.h"
+
+
+//1,1 til 124,133
+//x-akse man kan bruge 2-132
+//y-akse man kan bruge 2-123
+void makeBoard(void) {
+	int32_t i;
+	fgcolor(3);
+	gotoxy(1,1);
+	printf("%c",218);
+	for(i = 0; i <= 134; i++) {
+		printf("%c",196);
+	}
+	printf("%c",191);
+	for(i = 2; i <= 123; i++) {
+		gotoxy(1,i);
+		printf("%c",179);
+		gotoxy(137,i);
+		printf("%c",179);
+	}
+	gotoxy(1,124);
+	printf("%c",192);
+	for(i = 0; i <= 134; i++) {
+		printf("%c",196);
+	}
+	printf("%c",217);
+}
+
