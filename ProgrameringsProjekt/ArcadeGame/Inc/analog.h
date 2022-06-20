206fb9334dcd41389661b6bace49f9f104023d88
s214399 <s214399@student.dtu.dk>
2022-06-20 12:47:54 +0200

    reset

--- ProgrameringsProjekt/ArcadeGame/Inc/analog.h
+++
@@ -1,9 +0,0 @@
-#ifndef ANALOG_H_
-#define ANALOG_H_
-
-#include "include.h"
-
-int8_t adcRead(void);
-int8_t buttonRead(void);
-
-#endif
--- ProgrameringsProjekt/ArcadeGame/Inc/collision.h
+++
@@ -1,9 +0,0 @@
-#ifndef COLLISION_H_
-#define COLLISION_H_
-
-#include "include.h"
-
-
-void collision(badShip a[], bigRock b[], goodShip *c);
-
-#endif /* COLLISION_H_ */
